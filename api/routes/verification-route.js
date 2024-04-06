b8114f6fc8845c0a597a828efeb821c845edfb87
VikiMaiselman <maiselmanwikki@gmail.com>
2024-04-06 22:59:56 +0300

    more logical way of implementing funcitonality

--- api/routes/verification-route.js
+++ api/routes/verification-route.js
@@ -9,47 +9,16 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
-// router.post("/sign-up", async (req, res) => {
-//   console.log("Hellow", req.body);
-
-//   try {
-//     // client.verify.v2.services.create({ friendlyName: "vikiSMSVerification" });
-//     // const verification = await client.verify.v2
-//     //   .services(process.env.TWILIO_SERVICE_SID)
-//     //   .verifications.create({ to: req.body.phone, channel: "sms" });
-//     // console.log(verification);
-//   } catch (error) {}
-
-//   const verification = { status: "pending" };
-//   return res.json(verification.status);
-// });
-
-// router.post("/verification", async (req, res) => {
-//   console.log(req.body);
-//   try {
-//     // const verificationCheck = await client.verify.v2
-//     //   .services(process.env.TWILIO_SERVICE_SID)
-//     //   .verificationChecks.create({ to: req.body.phone, code: req.body.otp });
-//     // console.log(verificationCheck.status);
-//     // if (verificationCheck.status !== "approved") {
-//     //   throw new "Invalid OTP!"();
-//     // }
-//   } catch (error) {
-//     return res.json(false);
-//   }
-
-//   const { action } = req.body;
-//   action === "signup" ? register(req, res) : login(req, res);
-
-//   //   return res.json("approved");
-//   return res.json(true);
-// });
-
 router.post("/sign-up", async (req, res, next) => {
   const { username, password, phone, action } = req.body;
 
+  // const createService = async (req, res) => {
+  //   client.verify.v2.services.create({ friendlyName: "phoneVerification" }).then((service) => console.log(service.sid));
+  // };
+
   // a method provided by the package, abstracts away our interaction with DB
   if (action === "signup") {
+    let verification;
     User.register({ username: username, phone: phone }, password, async function (err, user) {
       if (err) {
         console.error(err);
@@ -57,34 +26,27 @@ router.post("/sign-up", async (req, res, next) => {
         return;
       }
 
-      console.log("hello user", user);
+      console.log(phone);
       try {
-        // const verification = await client.verify.v2
-        //   .services(process.env.TWILIO_SERVICE_SID)
-        //   .verifications.create({ to: req.body.phone, channel: "sms" });
-        // console.log(verification);
+        verification = await client.verify.v2
+          .services(process.env.TWILIO_SERVICE_SID)
+          .verifications.create({ to: phone, channel: "sms" });
+        console.log(verification.status);
+        return res.json(verification.status);
       } catch (error) {
-        return res.json(false);
+        console.log(error);
+        return res.status(error.status).send(error);
       }
-      const verification = { status: "pending" };
-      return res.json(verification.status);
-      // if the user was successfully authenticated
-      // passport.authenticate("local")(req, res, async function () {
-      //   return;
-      // });
     });
   }
 
   if (action === "login") {
-    const user = new User({
-      username: username,
-      password: password,
-    });
-
-    console.log("User.login", User.login);
-
+    let verification;
+    let userPhoneStoredInDB;
+    // console.log(username, password, phone, action, req.user);
     try {
       const result = await User.authenticate()(username, password);
+      userPhoneStoredInDB = result.user.phone;
       if (result.error) throw new Error("IncorrectPasswordError: Password or username is incorrect");
     } catch (error) {
       console.log(error);
@@ -93,37 +55,39 @@ router.post("/sign-up", async (req, res, next) => {
     }
 
     try {
-      // const verification = await client.verify.v2
-      //   .services(process.env.TWILIO_SERVICE_SID)
-      //   .verifications.create({ to: req.body.phone, channel: "sms" });
-      // console.log(verification);
+      verification = await client.verify.v2
+        .services(process.env.TWILIO_SERVICE_SID)
+        .verifications.create({ to: userPhoneStoredInDB, channel: "sms" });
+      console.log(verification);
     } catch (error) {
-      return res.json(false);
+      res.status(401).send(false);
     }
-    const verification = { status: "pending" };
+    // console.log(verification);
+    // const verification = { status: "pending" };
     return res.json(verification.status);
   }
 });
 
 router.post("/verification", async (req, res) => {
+  let verificationCheck;
   const { username, password, phone, action, otp } = req.body;
   console.log("in verification", username, password, phone, action, otp);
 
   try {
-    // const verificationCheck = await client.verify.v2
-    //   .services(process.env.TWILIO_SERVICE_SID)
-    //   .verificationChecks.create({ to: phone, code: otp });
-    // console.log(verificationCheck.status);
-    // if (verificationCheck.status !== "approved") {
-    //   throw new "Invalid OTP!"();
-    // }
+    verificationCheck = await client.verify.v2
+      .services(process.env.TWILIO_SERVICE_SID)
+      .verificationChecks.create({ to: phone, code: otp });
+    console.log(verificationCheck.status);
+    if (verificationCheck.status !== "approved") {
+      throw new "Invalid OTP!"();
+    }
   } catch (error) {
-    return res.json(false);
+    return res.status(401).send(false);
   }
 
   passport.authenticate("local")(req, res, function () {
     console.log("verify", req.isAuthenticated());
-    res.send("approved");
+    res.send(verificationCheck.status);
   });
 
   //   req.login(user, function (err) {
@@ -155,11 +119,5 @@ const getStatus = async (req, res) => {
 // export const getCurrentUser = async (req, res) => {
 //   res.send({ id: req.user?._id, username: req.user?.username });
 // };
-
-// router.post("/register", register);
-// router.post("/login", login);
-// router.get("/logout", logout);
-// router.get("/authstatus", getStatus);
-// router.get("/get-user", getCurrentUser);
 
 export { router as verificationRoutes };
