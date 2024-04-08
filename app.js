0eec67950682dff97294034b2139308a8a440ae2
VikiMaiselman <maiselmanwikki@gmail.com>
2024-04-08 22:13:42 +0300

    revert and change 4

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ app.use(
     saveUninitialized: false,
     // store: new RedisStore(options),
     cookie: {
-      // httpOnly: false,
+      httpOnly: false,
       secure: true,
 
       // secure: true, // set this to true if you're using https
-      sameSite: "none", // set this to 'none' if your site is served over https
+      sameSite: true, // set this to 'none' if your site is served over https
       // other cookie settings
     },
   })
