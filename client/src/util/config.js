248be12bfe7abdbcf96575d1fa5199eb103bb05f
VikiMaiselman <maiselmanwikki@gmail.com>
2024-04-08 18:56:24 +0300

    client in different folder

--- client/src/util/config.js
+++
@@ -1,7 +0,0 @@
-export const URL = "http://localhost:3010";
-export const HEADERS = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Origin": "http://localhost:3000",
-};
-
-export const DRAWER_WIDTH = 250;
